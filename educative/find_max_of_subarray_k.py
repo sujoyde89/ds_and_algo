025ab2053f3c67befdb8a5c0c5c1aea29ccf6e89
Sujoy De <sujoy119@gmail.com>
2022-01-28 23:31:05 +0530

    subarray

--- educative/find_max_of_subarray_k.py
+++ educative/find_max_of_subarray_k.py
@@ -1,18 +1,17 @@
-def find_averages_of_sub_arrays(k, arr):
-    averages = []
+def max_sub_array_of_size_k(k, arr):
     window_sum = 0
+    max_window_sum = 0
     for i in range(len(arr)):
         window_sum += arr[i]
         if i>=k-1:
-            averages.append(window_sum/k)
+            max_window_sum = max(max_window_sum, window_sum)
             window_sum -= arr[i-k+1]
-    return averages
-            
+    return max_window_sum
 
 def main():
     arr = [2, 1, 5, 1, 3, 2]
     k = 3
-    result = find_averages_of_sub_arrays(k , arr)
+    result = max_sub_array_of_size_k(k, arr)
     print("Averages of subarrays of size K: " + str(result))
 
 main()
