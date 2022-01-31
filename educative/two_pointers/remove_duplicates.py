1013d14a427202102cd464a9b6abb7b6a8918d67
Sujoy De <sujoy119@gmail.com>
2022-01-31 22:34:18 +0530

    Remove duplicates

---
+++ educative/two_pointers/remove_duplicates.py
@@ -0,0 +1,23 @@
+# non duplicate index 1
+# index 1
+# increment index
+# if duplicate do nothing increment index but dont do anything with non duplicate
+# if non duplicate replace element of index with non duplicate
+
+def remove_duplicates(arr):
+    i = 1
+    next_non_duplicate_index = 1
+    while i<len(arr):
+        if arr[i]!=arr[next_non_duplicate_index-1]:
+            arr[next_non_duplicate_index] = arr[i]
+            next_non_duplicate_index+=1
+        i+=1
+    return next_non_duplicate_index
+
+
+def main():
+  print(remove_duplicates([2, 3, 3, 3, 6, 9, 9]))
+  print(remove_duplicates([2, 2, 2, 11]))
+
+
+main()
